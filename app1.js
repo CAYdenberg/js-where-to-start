92e1a306bcc536521b1fc60f230250c2d7608413
@CAYdenberg <ydenberg@gmail.com>
2017-10-21 19:49:43 -0600

    Tweaks

--- app1.js
+++ app1.js
@@ -10,8 +10,11 @@ function makeRequest(callback) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  // When the button is clicked ...
   document.getElementById('startRequest').addEventListener('click', function() {
+    // get the data ...
     makeRequest(function(response) {
+      // and put the temperature on the page
       const currently = response.currently
       const itIs = 'It is ' + currently.summary + ' and the temperature is ' + currently.temperature + ' C'
       document.getElementById('weather').innerHTML = itIs
--- app2.js
+++ app2.js
@@ -3,23 +3,56 @@
 function sendChat(payload, callback) {
   var requestOptions = {
     url: 'http://localhost:3000/chat',
-    method: 'POST',
     contentType: 'application/json',
+    method: 'POST',
     data: JSON.stringify(payload),
     success: callback
   }
-  console.log(requestOptions)
   $.ajax(requestOptions)
 }
 
+function getChats(callback) {
+  var requestOptions = {
+    url: 'http://localhost:3000/chat',
+    contentType: 'application/json',
+    method: 'GET',
+    success: callback
+  }
+  $.ajax(requestOptions)
+}
+
+// translate the an array of chats (retrieved from API)
+// into a list on the page
+function renderChats(chats) {
+  var chatsElem = document.getElementById('chats')
+  chatsElem.innerHTML = ''
+  chats.forEach(function(chat) {
+    var newElem = document.createElement('li')
+    var textNode = document.createTextNode(chat)
+    newElem.appendChild(textNode)
+    document.getElementById('chats').appendChild(newElem)
+  })
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+  // get chats from API and render them
+  getChats(renderChats)
+
+  // when the button is clicked ...
   document.getElementById('new-chat').addEventListener('submit', function(e) {
     e.preventDefault()
+    // get the contents of the text box
     var input = document.getElementById('new-chat-input')
     var text = input.value
+    // send it to the API
     sendChat({chat: text}, function(res) {
+      // rerender the chats
       input.value = ''
-      console.log(res)
+      renderChats(res)
     })
   })
+
+  setInterval(function() {
+    getChats(renderChats)
+  }, 1000)
 })
